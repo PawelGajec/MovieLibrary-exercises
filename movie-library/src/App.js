5ac3e87b84adddfdbf07cf05109251b7c9c6bd8b
Paweł Gajec <103367076+PawelGajec@users.noreply.github.com>
2023-03-04 12:31:42 +0100

    play with JS

--- movie-library/src/App.js
+++ movie-library/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 
 const App =() => {
     const name = 'Paweł'
+    const lastName ='Gajec'
   return (
     <div className="App">
-      <h1>Hello {name} </h1>
+      <h1>Hello {name + ' ' + lastName} </h1>
     </div>
   );
 }
